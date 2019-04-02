f2eea5f26dc18e0f90254063b881cfacadd44c17
Ben Halverson <benhalverson33@gmail.com>
2019-04-01 22:26:31 -0700

    wip refactor

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,11 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
-import 'hero.dart';
+import 'src/hero.dart';
 
-@Component(selector: 'my-app', directives: [formDirectives], template: '''
-  <div>
-  <label>name: </label>
-  <input [(ngModel)]="hero.name" placeholder="name">
-</div>
-  ''')
+@Component(
+    selector: 'my-app',
+    templateUrl: './app_component.html',
+    directives: [formDirectives])
 class AppComponent {
   final title = 'Twitch';
   Hero hero = Hero(1, 'Windstorm');
--- test/app_test.dart
+++ test/app_test.dart
@@ -21,7 +21,7 @@ void main() {
   });
 
   test('Greet twitch', () async {
-    await fixture.update((c) => c.name = 'World');
+    await fixture.update((c) => c.hero = {1, 'Winstorm'});
     expect(fixture.text, 'Hello World');
   });
 
